a4c812aaf39d108ee6b8283256c313f1ffa18afd
sinalsal <86508113+sinalsal@users.noreply.github.com>
2021-09-13 11:19:41 +0700

    Halaman daftar

---
+++ resources/views/daftar.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar | PPDB</title>
+</head>
+<body>
+    <div>
+        
+        <h2>Masuk</h2>
+        <form action="">
+            <input type="text">
+            <input type="text">
+            <p>Sudah punya akun ?</p><a href="/Masuk">Masuk</a>
+            <br>
+            <input type="button" value="Daftar">
+        </form>
+    </div>
+</body>
+</html>
