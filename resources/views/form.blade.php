cb1431a9a80f3a6ff0878824f1b49326221057c2
sinalsal <86508113+sinalsal@users.noreply.github.com>
2021-09-13 11:22:21 +0700

    Halaman Formulir

---
+++ resources/views/form.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulir Pendaftaran | PPDB</title>
+</head>
+<body>
+    <div>
+        
+        <h2>Formulir</h2>
+        <form action="">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <br>
+            <input type="button" value="Kirim">
+        </form>
+    </div>
+</body>
+</html>
